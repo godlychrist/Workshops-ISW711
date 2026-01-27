432293144213e8003965544462ad44f56bcb1724
Cristopher <cristopherquiros2@gmail.com>
2026-01-27 15:14:52 -0600

    Workshop 1 Done

---
+++ Workshop-1/index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require('./routes/routes');
+
+const app = express();
+app.use(express.json());
+app.use('/api', routes)
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
+
+const mongoString = process.env.MONGODB_URI
+
+mongoose.connect(mongoString);
+const database = mongoose.connection
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
